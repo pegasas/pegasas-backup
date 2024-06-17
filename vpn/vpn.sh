02ebdf578c3e2b1a407d3cb6fa6c440b4fae12ec
pegasas <pegasashjy@gmail.com>
2024-06-17 03:03:55 -0400

    20240617.1

--- vpn/vpn.sh
+++ vpn/vpn.sh
@@ -1,15 +1,1 @@
-#!/bin/sh -e
-#
-# rc.local
-#
-# This script is executed at the end of each multiuser runlevel.
-# Make sure that the script will "exit 0" on success or any other
-# value on error.
-#
-# In order to enable or disable this script just change the execution
-# bits.
-#
-# By default this script does nothing.
-# nohup /home/huangjunyao/v2ray-linux-64/v2ray run > /home/huangjunyao/vpn.log 2>&1 &
-nohup /home/huangjunyao/Xray-linux-64/xray -c /home/huangjunyao/Xray-linux-64/config.json > /home/huangjunyao/vpn.log 2>&1 &
-exit 0
+nohup /home/huangjunyao/vpn/Xray-linux-64/xray -c /home/huangjunyao/vpn/Xray-linux-64/config.json > /home/huangjunyao/vpn/Xray-linux-64/vpn.log 2>&1 &
